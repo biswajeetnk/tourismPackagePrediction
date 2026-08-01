a3cef2c9e6d7607cf38a0bfb900775668ddecde0
Biswajeet Nayak <bnnayakbiswajeet@gmail.com>
2026-08-02 01:25:06 +0530

    Update validate_data.py

--- src/validate_data.py
+++ src/validate_data.py
@@ -1,1 +1,44 @@
+import pandas as pd
 
+# Load dataset
+df = pd.read_csv("data/tourism.csv")
+
+# Expected columns
+expected_columns = [
+    "CustomerID",
+    "ProdTaken",
+    "Age",
+    "TypeofContact",
+    "CityTier",
+    "DurationOfPitch",
+    "Occupation",
+    "Gender",
+    "NumberOfPersonVisiting",
+    "NumberOfFollowups",
+    "ProductPitched",
+    "PreferredPropertyStar",
+    "MaritalStatus",
+    "NumberOfTrips",
+    "Passport",
+    "PitchSatisfactionScore",
+    "OwnCar",
+    "NumberOfChildrenVisiting",
+    "Designation",
+    "MonthlyIncome"
+]
+
+# Validate columns
+if list(df.columns) == expected_columns:
+    print("Dataset validation successful.")
+else:
+    raise Exception("Dataset validation failed. Column mismatch.")
+
+# Print summary
+print("\nDataset Summary")
+print("---------------------------")
+print(f"Rows    : {df.shape[0]}")
+print(f"Columns : {df.shape[1]}")
+print("\nMissing Values")
+print(df.isnull().sum())
+print("\nData Types")
+print(df.dtypes)
